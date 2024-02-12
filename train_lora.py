4c42dfdc17f20408400991163717d67545e71aa5
Angel Rodriguez Junquera <angel@sublimestudio.ch>
2024-02-12 16:14:59 +0100

    Update

--- train_lora.py
+++ train_lora.py
@@ -21,12 +21,12 @@ def create_training_script(train_folder, model_path):
     --unet_lr=0.0004 
     --network_dim=1024
     --output_name="last" 
-    --max_train_epochs=20
+    --max_train_epochs=60
     --no_half_vae 
     --learning_rate=0.0004
     --lr_scheduler="constant" 
     --train_batch_size="1" 
-    --save_every_n_epochs="1" 
+    --save_every_n_epochs="10" 
     --mixed_precision="bf16" 
     --save_precision="bf16" 
     --cache_latents 
