8fb05d55cb09394e90a165c1e33c9240ca664725
Angel Rodriguez Junquera <angel@sublimestudio.ch>
2024-02-23 09:51:53 +0100

    Update

--- detect.py
+++ detect.py
@@ -4,6 +4,7 @@ from PIL import Image, ImageDraw, ImageOps
 import numpy as np
 import torch
 import cv2
+from torchvision.transforms import ToPILImage
 
 # Ensure the script's directory is in the Python path for module imports
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -12,15 +13,72 @@ sys.path.append(script_dir)
 # Import the necessary functions from the provided code snippet
 from node import load_groundingdino_model, load_sam_model, groundingdino_predict, sam_segment
 
-def detect_and_segment_boxes(image_path, grounding_dino_model_name, sam_model_name, segmentation_class, threshold, crop_resolution,  train_repeats, train_class, output_folder):
-   
+def detect_box(grounding_dino_model_name, image, segmentation_class, threshold):
+
     def extract_biggest_box(boxes):
         box_areas = (boxes[:, 2] - boxes[:, 0]) * (boxes[:, 3] - boxes[:, 1])
         biggest_box_idx = torch.argmax(box_areas)
         return boxes[biggest_box_idx]
+    
+    grounding_dino_model = load_groundingdino_model(grounding_dino_model_name)
+    item = image.convert('RGBA')
+    boxes = groundingdino_predict(grounding_dino_model, item, segmentation_class, threshold)
+    print(f"Detected {boxes.size(dim=0)} boxes.")
+    return extract_biggest_box(boxes)
+
+def segment(sam_model_name, image, torch_box):
+    sam_model = load_sam_model(sam_model_name)
+    item = image.convert('RGBA')
+    (images, masks) = sam_segment(
+                sam_model,
+                item,
+                torch_box
+            )
+    #TODO: Check if more than one mask or if any
+    return ToPILImage()(images[0].squeeze(0).permute(2, 0, 1))
+
+def crop_and_resize(image, box, crop_resolution):
+
+    image = image.crop(box.numpy())
+
+    #resize keeping original highest dimension as crop_resolution
+    width, height = image.size
+    if width > height:
+        new_width = crop_resolution
+        new_height = int(height * (crop_resolution / width))
+    else:
+        new_height = crop_resolution
+        new_width = int(width * (crop_resolution / height))
+    return image.resize((new_width, new_height))
+
+    # image.save(os.path.join(output_folder, f"crop_{crop_resolution}.jpg"))
+
+    # canny = canny_edge_detector(image, 1)
+    # canny.save(os.path.join(output_folder, f"canny_{crop_resolution}.jpg"))
+
+
+
+def detect_and_segment_boxes(image_path, grounding_dino_model_name, sam_model_name, segmentation_class, threshold, crop_resolution,  train_repeats, train_class, output_folder):
+    
+    def fillWithColor(image):
+        image_np = np.array(image)
+
+        # Generate a random RGB color
+        random_color = np.random.randint(0, 256, size=(3,), dtype=np.uint8)
+
+        # Find all black pixels (where all RGB values are 0)
+        black_pixels_mask = (image_np[:, :, 0] == 0) & (image_np[:, :, 1] == 0) & (image_np[:, :, 2] == 0)
+
+        # Replace black pixels with the random color
+        image_np[black_pixels_mask] = random_color
+
+        # Convert the modified array back to a PIL image
+        return Image.fromarray(image_np)
+
+    
     # Load models
     grounding_dino_model = load_groundingdino_model(grounding_dino_model_name)
-    # sam_model = load_sam_model(sam_model_name)
+    sam_model = load_sam_model(sam_model_name)
 
     # Load and process the image
     image = Image.open(image_path)
@@ -30,9 +88,9 @@ def detect_and_segment_boxes(image_path, grounding_dino_model_name, sam_model_na
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
     image.save(os.path.join(output_folder, "original.jpg"))
-
+    item = image.convert('RGBA')
     # # Predict boxes using GroundingDINO model
-    boxes = groundingdino_predict(grounding_dino_model, image.convert("RGBA"), segmentation_class, threshold)
+    boxes = groundingdino_predict(grounding_dino_model, item, segmentation_class, threshold)
     print(boxes)
     # Create an ImageDraw object to draw on the image
 
@@ -41,7 +99,25 @@ def detect_and_segment_boxes(image_path, grounding_dino_model_name, sam_model_na
     if boxes.size(0) == 0:
         return
     
-    biggest_box = extract_biggest_box(boxes).numpy()
+    
+    torch_biggest_box = extract_biggest_box(boxes)
+
+    (images, masks) = sam_segment(
+                sam_model,
+                item,
+                torch_biggest_box
+            )
+
+    # Convert the mask to a PIL image
+    for i, image in enumerate(images):
+        print(f"Saving mask {image.shape}.")
+        pil_image = ToPILImage()(image.squeeze(0).permute(2, 0, 1))
+        pil_image = fillWithColor(pil_image)
+        pil_image.save(os.path.join(output_folder, f"mask_{i}.jpg"))
+        # save_image(image.squeeze(0), os.path.join(output_folder, "masked.jpg"))
+
+    biggest_box = torch_biggest_box.numpy()
+
     print("Using biggest box: ", biggest_box)
 
     # crop image to bigges box
@@ -101,6 +177,25 @@ def canny_edge_detector(image, dilation):
     # return PIL image
     return Image.fromarray(dilated_edges_rgb)
 
+def augment_with_background_color(image, red, blue, green):
+    image_np = np.array(image)
+    black_pixels_mask = (image_np[:, :, 0] == 0) & (image_np[:, :, 1] == 0) & (image_np[:, :, 2] == 0)
+    image_np[black_pixels_mask] = [red, blue, green]
+    return Image.fromarray(image_np)
+    
+def save_image_in_train_folder(image, repeats, name):
+    train_img_folder = os.path.join(output_folder, "train_img")
+    if not os.path.exists(train_img_folder):
+        os.makedirs(train_img_folder)
+
+    train_class_joined = "_".join(name.split(" "))
+    image_class_folder = os.path.join(train_img_folder, f"{repeats}_{train_class_joined}")
+    if not os.path.exists(image_class_folder):
+        os.makedirs(image_class_folder)
+    
+    image.save(os.path.join(image_class_folder, f"{train_class_joined}.jpg"))
+
+
 if __name__ == "__main__":
     image_path = sys.argv[1]
     crop_resolution = int(sys.argv[2])
@@ -112,6 +207,41 @@ if __name__ == "__main__":
     threshold = 0.5  # Detection threshold
     output_folder = sys.argv[3]  # Path for the output image
     train_repeats = 20
-    train_class = "TOKstyle silver earring held by a finger over a diffuse background"
+    train_class = "TOKstyle silver earring held by a finger"
 
-    detect_and_segment_boxes(image_path, grounding_dino_model_name, sam_model_name, segmentation_class, threshold, crop_resolution, train_repeats, train_class, output_folder)
+
+    image = Image.open(image_path)
+    image = ImageOps.exif_transpose(image)
+    torch_box = detect_box(grounding_dino_model_name, image, segmentation_class, threshold)
+    masked_image = segment(sam_model_name, image, torch_box)
+    cropped_image = crop_and_resize(image, torch_box, crop_resolution)
+    cropped_masked_image = crop_and_resize(masked_image, torch_box, crop_resolution)
+
+    canny = canny_edge_detector(cropped_image, 1)
+
+
+
+    masked_image.save(os.path.join(output_folder, "masked.jpg"))
+    image.save(os.path.join(output_folder, "original.jpg"))
+    cropped_image.save(os.path.join(output_folder, f"crop_{crop_resolution}.jpg"))
+    canny.save(os.path.join(output_folder, f"canny_{crop_resolution}.jpg"))
+
+    
+    save_image_in_train_folder(cropped_image, train_repeats, train_class)
+
+    augmented_images = 3
+
+    train_repeats_for_augmented = int(train_repeats / augmented_images)
+
+    blue_aug = augment_with_background_color(cropped_masked_image, 0, 0, 255)
+    save_image_in_train_folder(blue_aug, train_repeats_for_augmented, train_class + " over a blue background")
+    green_aug = augment_with_background_color(cropped_masked_image, 0, 255, 0)
+    save_image_in_train_folder(green_aug, train_repeats_for_augmented, train_class + " over a green background")
+    red_aug = augment_with_background_color(cropped_masked_image, 255, 0, 0)
+    save_image_in_train_folder(red_aug, train_repeats_for_augmented, train_class + " over a red background")
+    
+
+
+
+
+    # detect_and_segment_boxes(image_path, grounding_dino_model_name, sam_model_name, segmentation_class, threshold, crop_resolution, train_repeats, train_class, output_folder)
