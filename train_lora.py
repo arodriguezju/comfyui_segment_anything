9603f6b28b66fe7b97002dd7fdb4029f2f06ed4d
Angel Rodriguez Junquera <angel@sublimestudio.ch>
2024-03-04 10:39:45 +0100

    Update

--- train_lora.py
+++ train_lora.py
@@ -14,13 +14,13 @@ def create_training_script(train_folder, model_path):
     --train_data_dir="{train_folder}" 
     --resolution="512,512" 
     --output_dir="/workspace/output_training" 
-    --network_alpha=1
+    --network_alpha=64
     --save_model_as=safetensors 
     --network_module=networks.lora 
     --text_encoder_lr=0.00005
     --unet_lr=0.0001
     --network_dim=1024
-    --output_name="epicr-earring-augmBg-1024-lr0.0001-0.00005-a1-3" 
+    --output_name="epicr-earring-augmBg-1024-lr0.0001-0.00005-a64" 
     --max_train_epochs=50
     --no_half_vae 
     --lr_scheduler="constant" 
