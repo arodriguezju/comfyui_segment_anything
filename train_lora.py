b10bc76652def1264a1bb372d41b8827e8abf6a7
Angel Rodriguez Junquera <angel@sublimestudio.ch>
2024-03-05 09:05:38 +0100

    Update

--- train_lora.py
+++ train_lora.py
@@ -21,7 +21,7 @@ def create_training_script(train_folder, model_path):
     --unet_lr=0.0001
     --network_dim=1024
     --output_name="epicr-earring-augmBg-1024-lr0.0001-0.00005-a128" 
-    --max_train_epochs=50
+    --max_train_epochs=80
     --no_half_vae 
     --lr_scheduler="constant" 
     --train_batch_size="1" 
